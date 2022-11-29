2af24a888695e34aa9f83450d0fc2f47612a55c3
nathaniel78 <nathanieljose78@hotmail.com>
2022-11-28 21:34:10 -0400

    Inicio do projeto

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+#from django.contrib.localflavor.br.models import STATE_CHOICES
+
+# Create your models here.
+
+class EnderecoCliente(models.Model):
+    STATE_CHOICES = (
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins')
+    )
+
+    rua = models.CharField(max_length=50, null=False, blank=False)
+    cidade = models.CharField(max_length=30, null=False, blank=False)
+    estado = models.CharField(max_length=2, choices=STATE_CHOICES, null=False, blank=False)
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    endereco = models.ForeignKey(EnderecoCliente, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=14, null=False, blank=False)
+    data_nascimento = models.DateField(null=False, blank=False)
+    profissao = models.CharField(max_length=25, null=False, blank=False)
+
+
